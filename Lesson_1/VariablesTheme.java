262a68ebc4a143dbc81b4594ca72d7501a133987
ArtemLearning <ArtemLearning@yandex.ru>
2022-09-16 15:29:01 +0300

    Commit v. 0.1

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -3,7 +3,6 @@ public class VariablesTheme
 
     public static void main(String[] args)
     {
-
         System.out.println("Пункт 1");
 
         byte    byteCores = 2;
@@ -25,6 +24,47 @@ public class VariablesTheme
         System.out.println("Является ли рабочий компьютер ноутбуком? - " + boolIsNote);
 
 
+        System.out.println("\nПункт 2");
 
+        int penPrice = 100;
+        int bookPrice = 200;
+        int discountPercent = 11;
+
+        float disountValue = ( ( penPrice + bookPrice ) / 100 ) * discountPercent;
+        System.out.println("Сумма скидки - " + disountValue + " руб.");
+        float totalSum = ( penPrice + bookPrice ) - disountValue;
+        System.out.println("Общая стоимость товаров со скидкой - " + totalSum + " руб.");
+
+
+        System.out.println("\nПункт 3");
+
+        System.out.println("   J    a  v     v  a    ");
+        System.out.println("   J   a a  v   v  a a   ");
+        System.out.println("J  J  aaaaa  V V  aaaaa ");
+        System.out.println(" JJ  a     a  V  a     a");
+
+
+        System.out.println("\nПункт 4");
+        byteCores = 127;
+        shortWindowsVersion = 32767;
+        intResolution = 2147483647;
+
+        System.out.println("Начальное значение переменной типа byte - " + byteCores);
+        byteCores += 1;
+        System.out.println("Значение переменной типа byte + 1 - " + byteCores);
+        byteCores -= 1;
+        System.out.println("Значение переменной типа byte - 1 - " + byteCores);
+
+        System.out.println("Начальное значение переменной типа short - " + shortWindowsVersion);
+        shortWindowsVersion += 1;
+        System.out.println("Значение переменной типа short + 1 - " + shortWindowsVersion);
+        shortWindowsVersion -= 1;
+        System.out.println("Значение переменной типа short - 1 - " + shortWindowsVersion);
+
+        System.out.println("Начальное значение переменной типа int - " + intResolution);
+        intResolution += 1;
+        System.out.println("Значение переменной типа int + 1 - " + intResolution);
+        intResolution -= 1;
+        System.out.println("Значение переменной типа int - 1 - " + intResolution);                
     }
 }
