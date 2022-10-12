67f5229e3ba809a21819b12d364c83a1eb0aecf4
ArtemLearning <ArtemLearning@yandex.ru>
2022-10-12 13:36:47 +0300

    finished CycleTheme

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -42,7 +42,7 @@ public class CyclesTheme {
         System.out.print(i + " ");
     }
 
-    System.out.println("\n3. Вывод реверсивного числа и суммы его цифр");
+    System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр");
     int testVar = 1234;
     int thousands = 0;
     int hundreds = 0;
@@ -79,6 +79,101 @@ public class CyclesTheme {
         System.out.printf("%2d ", 0);
         iterator++;
     } while (iterator < 5);
+
+    System.out.println("\n\n5. Проверка количества единиц на четность");
+    testVar = 3141591;
+    int digit;
+    iterator = 0;
+
+    while (testVar > 0) {
+        digit = testVar % 10;
+        if (digit == 1) {
+            iterator++;
+        }
+        testVar = testVar / 10;
+    }
+    if (iterator % 2 == 0) {
+        System.out.println("Количество единиц = " + iterator + " чётное");
+    } else {
+        System.out.println("Количество единиц = " + iterator + " нечётное");
+    }
+
+    System.out.println("\n6. Отображение фигур в консоли\n");
+    int j;
+    for (i = 0; i < 5; i++ ) {
+        for (j=0; j < 10; j++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+    }
+
+    System.out.print("\n");
+    i = 5;
+    while (i <= 5 && i > 0) {
+        j = 0;
+        while (j < i) {
+            System.out.print("#");
+            j++;
+        }
+        System.out.print("\n");
+        i--;        
+    }
+
+    System.out.print("\n");
+    i = 5;
+    do {
+        if (i % 2 == 0) {
+            j = 2;            
+        } else if (i % 3 == 0) {
+            j = 3;
+        } else {
+            j = 1;
+        }
+        do {
+            System.out.print("$");
+            j--;
+        } while (j > 0);
+        System.out.print("\n");
+        i--;
+    } while (i <= 5 && i > 0);
+
+    System.out.println("\n7. Отображение ASCII-символов");
+    char digitAsText;
+    System.out.println("Dec\t" + "Char");
+    for (i = 0; i <= 47; i++) {
+        if (i % 2 != 0) {
+            digitAsText = (char) i;
+            System.out.printf("%3d",i);
+            System.out.printf("\t");
+            System.out.printf("%4c",digitAsText);
+            System.out.print("\n");
+        }
+    }
+    for (i=97 ;i < 123; i++ ) {
+        if (i % 2 == 0) {
+            digitAsText = (char) i;
+            System.out.printf("%3d",i);
+            System.out.printf("\t");
+            System.out.printf("%4c",digitAsText);
+            System.out.print("\n");
+        }
+    }
+
+    System.out.println("\n8. Проверка, является ли число палиндромом");
+    testVar = 1234321;
+    int digit1 = 0;
+    int digit2 = 0;
+    int digit3 = 0;
+    int digit4 = 0;
+    int digit5 = 0;
+    int digit6 = 0;
+    int digit7 = 0;
+    while (testVar > 0) {
+        digit1 = testVar % 10;
+        testVar = testVar / 10;
+    }
+
+
 
     }
 }
