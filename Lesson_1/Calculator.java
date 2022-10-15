6c9d2f7bc6470bb50375dfb89df662949a54d279
ArtemLearning <ArtemLearning@yandex.ru>
2022-10-15 21:25:28 +0300

    after second review

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,42 +1,42 @@
 public class Calculator {
 
     public static void main(String[] args) {
-    int calcNum1 = 123;
-    int calcNum2 = 12;
-    char operation = '/';
-    float result = 1f;
+        int a = 123;
+        int b = 12;
+        char operation = '/';
+        float result = 1f;
 
-    System.out.println("\nКалькулятор");
-    System.out.println("Число 1 = " + calcNum1);
-    System.out.println("Число 2 = " + calcNum2);
-    System.out.println("Операция - " + operation);
+        System.out.println("\nКалькулятор");
+        System.out.println("Число 1 = " + a);
+        System.out.println("Число 2 = " + b);
+        System.out.println("Операция - " + operation);
 
-    if (operation == '+') {
-        result = (float) calcNum1 + calcNum2;
-    } else if(operation == '-') {
-        result = (float) calcNum1 - calcNum2;
-    } else if(operation == '*') {
-        result = (float) calcNum1 * calcNum2;
-    } else if(operation == '/') {
-        if (calcNum2 !=0) {
-            result = (float) calcNum1 / calcNum2;
-        } else {
-            System.out.println("Деление на ноль!");
-            if (calcNum1 > 0) {
-                result = (float) Float.POSITIVE_INFINITY;
-            } else if (calcNum1 < 0) {
-            result = (float) Float.NEGATIVE_INFINITY;
+        if (operation == '+') {
+            result = (float) a + b;
+        } else if(operation == '-') {
+            result = (float) a - b;
+        } else if(operation == '*') {
+            result = (float) a * b;
+        } else if(operation == '/') {
+            if (a !=0) {
+                result = (float) a / b;
             } else {
-                result = (float) Float.NaN;
+                System.out.println("Деление на ноль!");
+                if (a > 0) {
+                    result = (float) Float.POSITIVE_INFINITY;
+                } else if (a < 0) {
+                result = (float) Float.NEGATIVE_INFINITY;
+                } else {
+                    result = (float) Float.NaN;
+                }
             }
-        }   
-    } else if(operation == '%') {
-        result = (float) calcNum1 % calcNum2;
-    } else if(operation == '^') {
-        for (int i = 0; i < calcNum2; i++) {
-            result = (float) result * calcNum1;
+        } else if(operation == '%') {
+            result = (float) a % b;
+        } else if(operation == '^') {
+            for (int i = 0; i < b; i++) {
+                result = (float) result * a;
+            }
         }
-    }
-    System.out.println(calcNum1 + " " + operation + " " + calcNum2 + " = " + result);
+        System.out.println(a + " " + operation + " " + b + " = " + result);
     }
 }
