c1948bd70d9b97049432f49be15bf80d93352253
ArtemLearning <ArtemLearning@yandex.ru>
2022-10-15 11:15:02 +0300

    after second review

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,31 +1,42 @@
 public class Calculator {
 
     public static void main(String[] args) {
-    int testVar1 = 123;
-    int testVar2 = 6;
+    int calcNum1 = 123;
+    int calcNum2 = 12;
     char operation = '/';
     float result = 1f;
 
     System.out.println("\nКалькулятор");
-    System.out.println("Число 1 = " + testVar1);
-    System.out.println("Число 2 = " + testVar2);
+    System.out.println("Число 1 = " + calcNum1);
+    System.out.println("Число 2 = " + calcNum2);
     System.out.println("Операция - " + operation);
 
     if (operation == '+') {
-        result = (float) testVar1 + testVar2;
+        result = (float) calcNum1 + calcNum2;
     } else if(operation == '-') {
-        result = (float) testVar1 - testVar2;
+        result = (float) calcNum1 - calcNum2;
     } else if(operation == '*') {
-        result = (float) testVar1 * testVar2;
+        result = (float) calcNum1 * calcNum2;
     } else if(operation == '/') {
-        result = (float) testVar1 / testVar2;
+        if (calcNum2 !=0) {
+            result = (float) calcNum1 / calcNum2;
+        } else {
+            System.out.println("Деление на ноль!");
+            if (calcNum1 > 0) {
+                result = (float) Float.POSITIVE_INFINITY;
+            } else if (calcNum1 < 0){
+            result = (float) Float.NEGATIVE_INFINITY;
+            } else {
+                result = (float) Float.NaN;
+            }
+        }   
     } else if(operation == '%') {
-        result = (float) testVar1 % testVar2;
+        result = (float) calcNum1 % calcNum2;
     } else if(operation == '^') {
-        for (int i = 0; i < testVar2; i++) {
-            result = (float) result * testVar1;
+        for (int i = 0; i < calcNum2; i++) {
+            result = (float) result * calcNum1;
         }
     }
-    System.out.println(testVar1 + " " + operation + " " + testVar2 + " = " + result);
+    System.out.println(calcNum1 + " " + operation + " " + calcNum2 + " = " + result);
     }
 }
