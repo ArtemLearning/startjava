14c95abcca63165066dd6d49d9dc71cef75f2b65
ArtemLearning <ArtemLearning@yandex.ru>
2022-10-15 10:55:20 +0300

    after second review

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -1,11 +1,11 @@
 public class IfElseStatementTheme {
 
     public static void main(String[] args) {
-
         System.out.println("\n1. Перевод псевдокода на язык Java");
         int age = 32;
         boolean isMale = false;
         float height = 1.92f;
+        char firstLetter = "Maxine".charAt(0);
 
         if (age > 20) {
             System.out.println("Уже можно покупать алкоголь");
@@ -22,9 +22,9 @@ public class IfElseStatementTheme {
         } else {
             System.out.println("Предложить баскетбольный мяч");
         }
-        if ("Maxine".charAt(0) == 'M') {
+        if (firstLetter == 'M') {
             System.out.println("Предложить именную ложку на букву М");
-        } else if ("Maxine".charAt(0) == 'I') {
+        } else if (firstLetter == 'I') {
             System.out.println("Предложить именную футболку");
         } else {
             System.out.println("Предложить общие вещи");
@@ -36,142 +36,57 @@ public class IfElseStatementTheme {
 
         if (srcNum1 > srcNum2) {
             System.out.println("Первое число " +
-                srcNum1 + " больше второго числа "
-                + srcNum2);
+                srcNum1 + " больше второго числа " + srcNum2);
         } else if (srcNum1 < srcNum2) {
-            System.out.println("Второе число " +
-                srcNum2 + " больше первого числа "
-                + srcNum1);
+            System.out.println("Второе число " + srcNum2 + " больше первого числа " + srcNum1);
         } else {
-            System.out.println("Числа " + srcNum1 +
-                " и " + srcNum2 + "равны");
+            System.out.println("Числа " + srcNum1 + " и " + srcNum2 + "равны");
         }
 
-
         System.out.println("\n3. Работа с числом");
-        int anyNumber = 33;
+        int anyNumber = 65;
 
-        switch (anyNumber) {
-            case 0:
-                System.out.println("Число " + anyNumber +
-                    " пустое");
-                break;
-            default:
-                if (anyNumber > 0) {
-                    System.out.println("Число " + anyNumber +
-                        " положительное");
-                } else {
-                    System.out.println("Число " + anyNumber +
-                        " отрицательное");
-                }
-                if ((anyNumber % 2) == 0) {
-                    System.out.println("Число " + anyNumber +
-                        " чётное");
-                } else {
-                    System.out.println("Число " + anyNumber +
-                        " нечётное");
-                }
+        if (anyNumber == 0) {
+            System.out.println("Число " + anyNumber + " пустое");
+        } else { 
+            if (anyNumber > 0) {
+                System.out.println("Число " + anyNumber + " положительное");
+            } else if (anyNumber < 0) {
+                System.out.println("Число " + anyNumber + " отрицательное");
+            }
+            if (anyNumber % 2 == 0) {
+                System.out.println("Число " + anyNumber + " чётное");
+            } else {
+                System.out.println("Число " + anyNumber + " нечётное");
+            }        
         }
 
         System.out.println("\n4. Поиск одинаковых цифр в числах");
         srcNum1 = 432;
         srcNum2 = 162;
 
-        System.out.println("Число 1 = " + srcNum1 +
-            "\nЧисло 2 = " + srcNum2);
+        System.out.println("Число 1 = " + srcNum1 + "\nЧисло 2 = " + srcNum2);
         if ((srcNum1 / 100) == (srcNum2 / 100)) {
-            System.out.println("Одинаковая цифра "
-                + (srcNum1 / 100) +
-                " в сотнях");
+            System.out.println("Одинаковая цифра " + (srcNum1 / 100) + " в сотнях");
         }
-        if (((srcNum1 % 100) / 10) == ((srcNum2 % 100) / 10)) {
-            System.out.println("Одинаковая цифра "
-                + ((srcNum1 % 100) / 10) +
-                " в десятках");
+        if (((srcNum1 % 100) / 10) == ((srcNum2 % 100) / 10)) { 
+            System.out.println("Одинаковая цифра " + ((srcNum1 % 100) / 10) + " в десятках");
         }
-        if (((srcNum1 % 100) % 10) == ((srcNum2 % 100) % 10)) {
-            System.out.println("Одинаковая цифра "
-                + ((srcNum1 % 100) % 10) +
-                " в единицах");
+        if ((srcNum1 % 10) == (srcNum2 % 10)) {
+            System.out.println("Одинаковая цифра " + (srcNum1 % 10) + " в единицах");
         }
 
         System.out.println("\n5. Определение буквы, числа или символа по их коду");
-
         char testCharVar = '\u0057';
 
-        switch (testCharVar) {
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                System.out.println("Символ " + testCharVar +
-                    " - это число");
-                break;
-            case 'a':
-            case 'b':
-            case 'c':
-            case 'd':
-            case 'e':
-            case 'f':
-            case 'g':
-            case 'h':
-            case 'i':
-            case 'k':
-            case 'l':
-            case 'm':
-            case 'n':
-            case 'o':
-            case 'p':
-            case 'q':
-            case 'r':
-            case 's':
-            case 't':
-            case 'u':
-            case 'v':
-            case 'w':
-            case 'x':
-            case 'y':
-            case 'z':
-                System.out.println("Символ " + testCharVar +
-                    " - это маленькая буква");
-                break;
-            case 'A':
-            case 'B':
-            case 'C':
-            case 'D':
-            case 'E':
-            case 'F':
-            case 'G':
-            case 'H':
-            case 'I':
-            case 'K':
-            case 'L':
-            case 'M':
-            case 'N':
-            case 'O':
-            case 'P':
-            case 'Q':
-            case 'R':
-            case 'S':
-            case 'T':
-            case 'U':
-            case 'V':
-            case 'W':
-            case 'X':
-            case 'Y':
-            case 'Z':
-                System.out.println("Символ " + testCharVar +
-                    " - это большая буква");
-                break;
-            default:
-                System.out.println("Символ " + testCharVar +
-                    " - это не буква и не число");
+        if(testCharVar >= '0' && testCharVar <= '9') {
+            System.out.println("Символ " + testCharVar + " - это число");
+        } else if (testCharVar >= 'a' && testCharVar <= 'z') {
+            System.out.println("Символ " + testCharVar + " - это маленькая буква");
+        } else if (testCharVar >= 'A' && testCharVar <= 'Z') {
+            System.out.println("Символ " + testCharVar + " - это большая буква");
+        } else {
+            System.out.println("Символ " + testCharVar + " - это не буква и не число");
         }
 
         System.out.println("\n6. Определение суммы вклада и начисленных банком %");
@@ -180,7 +95,7 @@ public class IfElseStatementTheme {
 
         if (sum <= 100000) {
             percent = sum * 0.05f;
-        } else if (sum > 100000 || sum <= 300000) {
+        } else if (sum > 100000 && sum <= 300000) {
             percent = sum * 0.07f;
         } else {
             percent = sum * 0.1f;
@@ -199,8 +114,6 @@ public class IfElseStatementTheme {
         int developmentMark;
         int middleMark;
         int middlePercent;
-
-        middlePercent = (historyPercent + developmentPercent) / 2;
 
         if (historyPercent <= 60) {
             historyMark = 2;
@@ -222,12 +135,13 @@ public class IfElseStatementTheme {
             developmentMark = 5;
         }
 
-        middleMark = (historyMark + developmentMark) / 2;
         System.out.println("Оценка\t" + "Предмет");
         System.out.println(historyMark + "\tИстория");
         System.out.println(developmentMark + "\tПрограммирование");
+        middleMark = (historyMark + developmentMark) / 2;
         System.out.println("Средний балл оценок - " + middleMark);
-        System.out.println("Средний процент - " + middlePercent);
+        middlePercent = (historyPercent + developmentPercent) / 2;
+        System.out.println("Средний процент - " + middlePercent + "%");
 
         System.out.println("\n8. Расчет прибыли");
         int rentPrice = 5000;
@@ -236,34 +150,31 @@ public class IfElseStatementTheme {
         int yearPrice;
         char sign = ' ';
 
-        rentPrice = rentPrice * 12;
-        productPrimeCost = productPrimeCost * 12;
-        productCost = productCost * 12;
-        yearPrice = productCost - (productPrimeCost + rentPrice);
+        yearPrice = (productCost * 12) - ((productPrimeCost * 12) + (rentPrice * 12));
         if (yearPrice > 0) {
             sign = '+'; 
         }
         System.out.println("Прибыль за год: " + sign + yearPrice);
 
-    System.out.println("\n9. Подсчет количества банкнот");
-    int cashNeeded = 567;
-    int hundreds;
-    int tens;
-    int units;
+        System.out.println("\n9. Подсчет количества банкнот");
+        int cashNeeded = 567;
+        int hundreds;
+        int tens;
+        int ones;
 
-    hundreds = cashNeeded / 100;
-    tens = (cashNeeded % 100) / 10;
-    units = (cashNeeded % 100) % 10;
+        hundreds = cashNeeded / 100;
+        tens = (cashNeeded % 100) / 10;
+        ones = (cashNeeded % 100) % 10;
     
-    if (tens > 5) {
-        units = units + (tens - 5) * 10;
-        tens = 5;
-    }
-    System.out.println("Номинал \t" + "Количество");
-    System.out.println("Сотни   \t" + hundreds);
-    System.out.println("Десятки \t" + tens);
-    System.out.println("Единицы \t" + units);
-    System.out.println("Исходная сумма = " + ((hundreds * 100) + (tens * 10) + units));
-    }
-    
+        if (tens > 5) {
+            ones = ones + (tens - 5) * 10;
+            tens = 5;
+        }
+        
+        System.out.println("Номинал \t" + "Количество");
+        System.out.println("Сотни   \t" + hundreds);
+        System.out.println("Десятки \t" + tens);
+        System.out.println("Единицы \t" + ones);
+        System.out.println("Исходная сумма = " + ((hundreds * 100) + (tens * 10) + ones));
+        }
 }
