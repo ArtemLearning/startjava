43cf081dec6e38c6a6deb783d40fa84a4f9cd7e0
ArtemLearning <ArtemLearning@yandex.ru>
2022-10-15 08:31:40 +0300

    after second review

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,20 +1,20 @@
 public class MyFirstGame {
-    public static void main(String[] args) {
 
-        int initialNumber = 34;
+    public static void main(String[] args) {
+        int targetNum = 34;
         int guessNumber = 0;
         int lowerLimit = 0;
 
-        while (guessNumber != initialNumber) {
-            if (guessNumber < initialNumber) {
+        while (guessNumber != targetNum) {
+            if (guessNumber < targetNum) {
                 guessNumber = lowerLimit + 5;
             }
-            if (guessNumber < initialNumber) {
-                System.out.println("Число " + guessNumber + " меньше задуманного");
+            if (guessNumber < targetNum) {
+                XSystem.out.println("Число " + guessNumber + " меньше задуманного");
                 lowerLimit = guessNumber + 5;
             } else {
                 System.out.println("Число " + guessNumber + " больше задуманного");
-                guessNumber = guessNumber - 1;
+                guessNumber--;
             }
         }
         System.out.println("Вы победили!");
