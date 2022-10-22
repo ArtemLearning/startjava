699505bd412cf35701f144253c92233066604715
ArtemLearning <ArtemLearning@yandex.ru>
2022-10-22 14:46:34 +0300

    First class

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    boolean isMale = true;
+    String name = "Joseph";
+    int height = 180;
+    int weight = 120;
+    int age = 35;
+
+    void walk() {
+        System.out.println("Walking");
+    }
+
+    void sit() {
+        System.out.println("Sitting");
+    }
+
+    void run() {
+        System.out.println("Running");
+    }
+
+    void talk() {
+        System.out.println("Talking");
+    }
+
+    void javaLearn() {
+        System.out.println("Learning Java");
+    }
+}
