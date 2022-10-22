dff588eca92e77af64f35681f2b2f0b05af985b8
ArtemLearning <ArtemLearning@yandex.ru>
2022-10-22 15:10:11 +0300

    First instance

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,10 +1,11 @@
 public class WolfTest {
     
     public static void main(String[] args) {
-    
     Wolf akella = new Wolf();
     akella.name = "Акелла";
-    System.out.println(akella.name + " " + akella.sit());
+    akella.color = "Палевый";
+    
+    System.out.println(akella.color + " " + akella.name + " " + akella.sit());
     if (akella.hunt()) {
         akella.howl();
     }
