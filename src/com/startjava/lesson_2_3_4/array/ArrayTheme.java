cca5487c5475c6bddb1b003db4c30eb777e78be1
ArtemLearning <ArtemLearning@yandex.ru>
2022-11-06 10:53:38 +0300

    Began hw for lesson_4 - 1. Swap array values 2. Multiply array values

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -5,22 +5,36 @@ public class ArrayTheme {
     public static void main(String[] args) {
 
         System.out.println("1. Реверс значений массива");
-        int[] numbers = { 4, 6, 3, 1, 2, 7, 5 };
-        for (int i : numbers) {
+        int[] numbers1 = { 4, 6, 3, 1, 2, 7, 5 };
+        for (int i : numbers1) {
             System.out.print(i + " ");
         }
-        for (int i = 0; i < numbers.length; i++) {
-            int j = numbers.length - (i + 1);
+        for (int i = 0; i < numbers1.length; i++) {
+            int j = numbers1.length - (i + 1);
             if (i == j) {
                 break;
             }
-            int swap = numbers[i];
-            numbers[i] = numbers[j];
-            numbers[j] = swap;
+            int swap = numbers1[i];
+            numbers1[i] = numbers1[j];
+            numbers1[j] = swap;
         }
         System.out.println("");
-        for (int i : numbers) {
+        for (int i : numbers1) {
             System.out.print(i + " ");
         }
+
+        System.out.println("\n2. Вывод произведения элементов массива");
+        int[] numbers2 = new int[10];
+        int multiplication = 1;
+        for (int i = 0; i < 10; i++) {
+            numbers2[i] = i;
+            System.out.print(numbers2[i] + " ");
+            if (numbers2[i] != 0 && numbers2[i] != 9) {
+                multiplication = multiplication * numbers2[i];
+            }
+        }
+        System.out.println("\n1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 = " + multiplication);
+        System.out.println(numbers2[0] + " " + numbers2[9]);
+
     }
 }
