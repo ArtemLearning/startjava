e67fa2b1ed6d290dc5e2c923df9ac886089357cf
ArtemLearning <ArtemLearning@yandex.ru>
2022-11-07 18:33:03 +0300

    реализуй вывод без String reverseAlphabet = ""; - используй вложенный цикл

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -3,79 +3,74 @@ package com.startjava.lesson_2_3_4.array;
 public class ArrayTheme {
 
     public static void main(String[] args) {
-
         System.out.println("1. Реверс значений массива");
-        int[] numbers1 = { 4, 6, 3, 1, 2, 7, 5 };
-
+        int[] intNumbers1 = {4, 6, 3, 1, 2, 7, 5};
         System.out.println("Исходный массив:");
-        printIntArray(numbers1);
-        for (int i = 0; i < numbers1.length; i++) {
-            int j = numbers1.length - (i + 1);
+        printIntArray(intNumbers1);
+        for (int i = 0; i < intNumbers1.length; i++) {
+            int j = intNumbers1.length - (i + 1);
             if (i == j) {
                 break;
             }
-            int swap = numbers1[i];
-            numbers1[i] = numbers1[j];
-            numbers1[j] = swap;
+            int swap = intNumbers1[i];
+            intNumbers1[i] = intNumbers1[j];
+            intNumbers1[j] = swap;
         }
-        System.out.println("\nИзменённый массив:");
-        printIntArray(numbers1);
+        System.out.println("Изменённый массив:");
+        printIntArray(intNumbers1);
+        System.out.println();
 
-        System.out.println("\n2. Вывод произведения элементов массива");
-        int[] numbers2 = new int[10];
-        int multiplication = 1;
+        System.out.println("2. Вывод произведения элементов массива");
+        int arrayLength = 10;
+        int[] intNumbers2 = new int[arrayLength];
+        int multiDigits = 1;
         for (int i = 0; i < 10; i++) {
-            numbers2[i] = i;
-            System.out.print(numbers2[i] + " ");
-            multiplication = (i != 0) ? (i != 9) ? (multiplication * numbers2[i]) : (multiplication) : (multiplication);
+            intNumbers2[i] = i;
+            multiDigits = (i != 0) ? (i != 9) ? (multiDigits * intNumbers2[i]) : (multiDigits) : (multiDigits);
         }
-        System.out.println("\n1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 = " + multiplication);
-        System.out.println(numbers2[0] + " " + numbers2[9]);
+        for (int i = 1; i < intNumbers2.length - 1; i ++) {
+            if (i == intNumbers2.length - 2) {
+                System.out.print(intNumbers2[i]);
+            } else {
+                System.out.print(intNumbers2[i] + " * ");
+            }
+        }
+        System.out.println(" = " + multiDigits);
+        System.out.println(intNumbers2[0] + " " + intNumbers2[9]);
+        System.out.println();
 
         System.out.println("3. Удаление элементов массива");
-        double[] doubleNumbers = new double[15];
-
+        arrayLength = 15;
+        double[] doubleNumbers = new double[arrayLength];
         for (int i = 0; i < doubleNumbers.length; i++) {
             doubleNumbers[i] = Math.random();
         }
         System.out.println("Исходный массив:");
-        for (int i = 0; i < doubleNumbers.length; i++) {
-            System.out.printf("%.3f", doubleNumbers[i]);
-            System.out.print(" ");
-            if (i == 7) {
-                System.out.println();
-            }
-        }
-        double middleValue = doubleNumbers[doubleNumbers.length / 2];
+        printDoubleArray(doubleNumbers);
+        double middleNumber = doubleNumbers[doubleNumbers.length / 2];
         int nullCounter = 0;
         for (int i = 0; i < doubleNumbers.length; i++) {
-            doubleNumbers[i] =  doubleNumbers[i] < middleValue ? 0 : doubleNumbers[i];
-            if (doubleNumbers[i] == 0) {
-                nullCounter += 1;
+            if (doubleNumbers[i] < middleNumber) {
+                doubleNumbers[i] = 0;
+                nullCounter++;
             }
         }
         System.out.print("\nЗначение в средней ячейке массива = ");
-        System.out.printf("%.3f", middleValue);
+        System.out.printf("%.3f", middleNumber);
         System.out.println("\nИзменённый массив:");
-        for (int i = 0; i < doubleNumbers.length; i++) {
-            System.out.printf("%.3f", doubleNumbers[i]);
-            System.out.print(" ");
-            if (i == 7) {
-                System.out.println();
-            }
-        }
+        printDoubleArray(doubleNumbers);
         System.out.println("\nЧисло нулей = " + nullCounter);
+        System.out.println();
 
         System.out.println("4. Вывод элементов массива лесенкой в обратном порядке");
         int charAmount = 'Z' - 'A' + 1;
-        char[] alphabetLetters = new char[charAmount];
-
-        for(int i = 0; i < charAmount ; i++){
-            alphabetLetters[i] = (char)('A' + i);
+        char[] engAlphabet = new char[charAmount];
+        for(int i = 0; i < charAmount; i++) {
+            engAlphabet[i] = (char) ('A' + i);
         }
         String reverseAlphabet = "";
         for (int i = charAmount - 1; i >= 0; i--) {
-            reverseAlphabet = reverseAlphabet + alphabetLetters[i];
+            reverseAlphabet = reverseAlphabet + engAlphabet[i];
             System.out.println(reverseAlphabet);
         }
 
@@ -94,7 +89,7 @@ public class ArrayTheme {
             for (int j = 0; j < 10; j++) {
                 System.out.print(uniqueNumbers[i * 10 + j] + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
         int[] uniqueSortNumbers = doSort(uniqueNumbers);
         System.out.println("Сортированный массив уникальных чисел:");
@@ -102,7 +97,7 @@ public class ArrayTheme {
             for (int j = 0; j < 10; j++) {
                 System.out.print(uniqueSortNumbers[i * 10 + j] + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
 
         System.out.println("\nЗадание со *");
@@ -130,10 +125,19 @@ public class ArrayTheme {
         for (int i : array) {
             System.out.print(i + " ");
         }
+        System.out.println();
     }
     private static void printStringArray(String[] array) {
         for (String s : array) {
             System.out.print(s + " ");
+        }
+    }
+    private static void printDoubleArray(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%.3f ", array[i]);
+            if (i == 7) {
+                System.out.println();
+            }
         }
     }
 
