600d4029c37f830955d29769dd5d4a915c318e73
ArtemLearning <ArtemLearning@yandex.ru>
2022-11-19 20:58:40 +0300

    19.11.2022 Final SQL statements after review

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,12 +1,13 @@
 CREATE DATABASE jaegers;
 \c jaegers;
-CREATE TABLE Jaegers (  id         SERIAL PRIMARY KEY,
-                        modelName  TEXT,
-                        mark       CHAR(6),
-                        height     NUMERIC(4, 2),
-                        weight     NUMERIC(4, 3),
-                        status     CHAR(10),
-                        origin     CHAR(10),
-                        launch     DATE,
-                        kaijuKill  INTEGER
-                     );
+CREATE TABLE Jaegers (
+    id         SERIAL PRIMARY KEY,
+    model_name TEXT,
+    mark       CHAR(6),
+    height     NUMERIC(4, 2),
+    weight     NUMERIC(4, 3),
+    status     CHAR(10),
+    origin     CHAR(10),
+    launch     DATE,
+    kaiju_kill INTEGER
+);
