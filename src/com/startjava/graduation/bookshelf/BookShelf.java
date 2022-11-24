bafb352d2014e3c95139f22fc1960b0d5843cf7e
ArtemLearning <ArtemLearning@yandex.ru>
2022-11-24 18:23:52 +0300

    24.11.2022 BookShelf definite final

--- src/com/startjava/graduation/bookshelf/BookShelf.java
+++ src/com/startjava/graduation/bookshelf/BookShelf.java
@@ -7,7 +7,7 @@ public class BookShelf {
     private static final int MAX_SPACE = 10;
     private final Book[] books = new Book[MAX_SPACE];
     private int quantityOfBooks;
-    private int maxLength;
+    private int maxLen;
 
     public Book[] getBooks() {
         return Arrays.copyOf(books, quantityOfBooks);
@@ -16,41 +16,41 @@ public class BookShelf {
     public void add(Book book) {
         if (book != null) {
             books[quantityOfBooks] = book;
-            if (maxLength < books[quantityOfBooks].getInfoLen()) {
-                maxLength = books[quantityOfBooks].getInfoLen();
+            if (maxLen < books[quantityOfBooks].getInfoLen()) {
+                maxLen = books[quantityOfBooks].getInfoLen();
             }
             quantityOfBooks++;
         }
     }
 
     public void delete(String name) {
-        try {
-            int bookNumber = findBookByName(name);
-            int bookLen = books[bookNumber].getInfoLen();
-            System.arraycopy(books, bookNumber + 1, books, bookNumber, quantityOfBooks - (bookNumber + 1));
+        int position = findBook(name);
+        if (position != -1) {
+            int bookLen = books[position].getInfoLen();
+            System.arraycopy(books, position + 1, books, position, quantityOfBooks - (position + 1));
             quantityOfBooks--;
-            if (maxLength == bookLen) {
+            if (maxLen == bookLen) {
                 calculateMaxLen();
             }
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
+        } else {
+            System.out.println("Нет книги с таким именем");
         }
     }
 
-    private int findBookByName(String name) {
+    public int findBook(String name) {
         for (int i = 0; i < quantityOfBooks; i++) {
             if (books[i].getName().equals(name)) {
                 return i;
             }
         }
-        throw new RuntimeException("Нет книги с таким именем.");
+        return -1;
     }
 
     private void calculateMaxLen() {
-        maxLength = 0;
+        maxLen = 0;
         for (int i = 0; i < quantityOfBooks; i++) {
-            if (maxLength < books[i].getInfoLen()) {
-                maxLength = books[i].getInfoLen();
+            if (maxLen < books[i].getInfoLen()) {
+                maxLen = books[i].getInfoLen();
             }
         }
     }
@@ -58,7 +58,7 @@ public class BookShelf {
     public void clearShelf() {
         Arrays.fill(books, 0, quantityOfBooks, null);
         quantityOfBooks = 0;
-        maxLength = 0;
+        maxLen = 0;
     }
 
     public int getQuantityOfBooks() {
@@ -69,7 +69,7 @@ public class BookShelf {
         return MAX_SPACE - getQuantityOfBooks();
     }
 
-    public int getMaxLength() {
-        return maxLength;
+    public int getMaxLen() {
+        return maxLen;
     }
 }
--- src/com/startjava/graduation/bookshelf/BookShelfTest.java
+++ src/com/startjava/graduation/bookshelf/BookShelfTest.java
@@ -9,15 +9,18 @@ public class BookShelfTest {
     static BookShelf myShelf = new BookShelf();
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int menuItem = 0;
-        while (menuItem != 4) {
+        while (menuItem != 5) {
             showBooks(myShelf.getBooks());
             showMenu();
-            menuItem = chooseMenuItem();
+            menuItem = chooseMenuItem(input);
+            input.nextLine(); //получение Enter
             switch (menuItem) {
-                case 1 -> addBook();
-                case 2 -> deleteBook();
-                case 3 -> myShelf.clearShelf();
+                case 1 -> addBook(input);
+                case 2 -> deleteBook(input);
+                case 3 -> findBook(input);
+                case 4 -> myShelf.clearShelf();
                 default -> System.out.println("Выход из программы");
             }
         }
@@ -26,17 +29,18 @@ public class BookShelfTest {
     private static void showBooks(Book[] books) {
         if (books != null) {
             for (Book book : books) {
-                System.out.println(printSingleBook(book.toString()));
+                printBook(book.toString());
             }
         }
     }
 
-    private static String printSingleBook(String str) {
-        String text = str + " ".repeat(myShelf.getMaxLength() - str.length());
-        String underscore = "_".repeat(myShelf.getMaxLength());
-        return """
+    private static void printBook(String info) {
+        String text = info + " ".repeat(myShelf.getMaxLen() - info.length());
+        String underscore = "_".repeat(myShelf.getMaxLen());
+        text = """
                 |%s|
                 |%s|""".formatted(text, underscore);
+        System.out.println(text);
     }
 
     private static void showMenu() {
@@ -45,54 +49,63 @@ public class BookShelfTest {
         System.out.println("Выберите действие:");
         System.out.println("1. Добавить книгу");
         System.out.println("2. Удалить книгу");
-        System.out.println("3. Очистить шкаф");
-        System.out.println("4. Завершить работу");
+        System.out.println("3. Найти книгу");
+        System.out.println("4. Очистить шкаф");
+        System.out.println("5. Завершить работу");
     }
 
-    private static int chooseMenuItem() {
-        Scanner input = new Scanner(System.in);
+    private static int chooseMenuItem(Scanner input) {
         try {
             return input.nextInt();
         } catch (InputMismatchException e) {
-            System.out.println("Введите число от 1 до 4");
-            return chooseMenuItem();
+            System.out.println("Введите число от 1 до 5");
+            return chooseMenuItem(input);
         }
     }
 
-    private static void addBook() {
+    private static void addBook(Scanner input) {
         if (myShelf.calculateFreeSpace() == 0) {
             System.out.println("Книжный шкаф заполнен полностью. Удалите книгу, либо очистите шкаф");
         }
         System.out.print("Введите название книги: ");
-        Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         System.out.print("Введите автора книги: ");
         String author = input.nextLine();
         System.out.print("Введите год издания: ");
-        Year year = inputYear(input.nextLine());
+        Year year = inputYear(input);
         myShelf.add(new Book(name, author, year));
     }
 
-    private static Year inputYear(String year) {
+    private static Year inputYear(Scanner input) {
+        String info = input.nextLine();
         try {
-            if (Year.parse(year).isAfter(Year.now())) {
-                return getCorrectYear();
+            if (Year.parse(info).isAfter(Year.now())) {
+                return fixYear(input);
             }
-            return Year.parse(year);
+            return Year.parse(info);
         } catch (DateTimeParseException e) {
-            return getCorrectYear();
+            return fixYear(input);
         }
     }
 
-    private static Year getCorrectYear() {
+    private static Year fixYear(Scanner input) {
         System.out.println("Введён неправильный год издания. Введите корректный год.");
-        Scanner input = new Scanner(System.in);
-        return inputYear(input.nextLine());
+        return inputYear(input);
     }
 
-    private static void deleteBook() {
+    private static void deleteBook(Scanner input) {
         System.out.println("Укажите название книги для удаления:");
-        Scanner input = new Scanner(System.in);
         myShelf.delete(input.nextLine());
+    }
+
+    private static void findBook(Scanner input) {
+        System.out.println("Укажите название книги для поиска:");
+        Book foundBook = myShelf.getBooks()[myShelf.findBook(input.nextLine())];
+        if (foundBook != null) {
+            printBook(foundBook.toString());
+        } else {
+            System.out.println("Книга не найдена.");
+        }
+        input.nextLine();
     }
 }
